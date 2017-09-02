a0f1c04c7af683d4bf27f624bea848558c90c0ad
carav008 <carav008@morris.umn.edu>
2017-09-02 15:46:11 -0500

    Made and finished writing wrap contents.sh
    Wraps contents of 1 file with 2 html files and puts it in another file.

---
+++ wrap_contents.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#assigning all variables 
+file=$1
+name=$2
+head='_header.html'
+foot='_footer.html'
+header=$name$head
+footer=$name$foot
+
+#Wraps contents of $file with $name and ouputs to newly made target file 
+touch $3
+cat $header $file $footer > $3
+
+
